7bfd2ab087a77a30cbd2c84bd78a809bc6473e32
Matthias Wuttke <matthias@wuttke.eu>
2016-10-27 10:49:56 +0200

    Add GWASQC step

---
+++ 06-gwasqc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SCRIPTS=/shared/cleaning/scripts
+WD=`pwd`
+
+rm -rf 06_gwasqc
+mkdir 06_gwasqc
+cd 06_gwasqc
+cp $SCRIPTS/gwasqc.R .
+cp $SCRIPTS/gwasqc-params-template.txt gwasqc-params.txt
+
+for FN in `ls -1 ../05_gwas_combined/*.gwas`
+do
+	BN=`basename $FN`
+	echo "PROCESS $BN" >> gwasqc-params.txt
+	ln -s $FN .
+done
+
+ls -l
+Rscript gwasqc.R
+
+cd $WD
