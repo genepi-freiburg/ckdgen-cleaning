b484fb0f64ecc1de62717f4f430250a1324ea128
Matthias Wuttke <matthias@wuttke.eu>
2016-12-06 10:46:06 +0100

    New script to find stratum by file name

---
+++ find-stratum.sh
@@ -0,0 +1,24 @@
+FN=$1
+if [ "$FN" == "" ]
+then
+	echo "Need filename"
+elif [[ $FN == *nonDM* ]]
+then
+        echo "nonDM"
+elif [[ $FN == *_DM* ]]
+then
+        echo "DM"
+elif [[ $FN == *_men* ]]
+then
+        echo "men"
+elif [[ $FN == *women* ]]
+then
+        echo "women"
+elif [[ $FN == *_overall_* ]]
+then
+        echo "overall"
+else
+	echo "???"
+fi
+
+
