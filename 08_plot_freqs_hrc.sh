21df067fcecb9a80cc628a3271fd0e5dc641c3ce
Matthias Wuttke <matthias@wuttke.eu>
2017-01-18 12:15:35 +0100

    Use absolute paths to be able to use script from different folders

--- 08_plot_freqs_hrc.sh
+++ 08_plot_freqs_hrc.sh
@@ -2,7 +2,7 @@ mkdir -p 08_plot_freqs
 for FN in `ls 05_gwas_combined/*gwas`
 do
 	echo "Check $FN with HRC"
-	../../scripts/join-to-hrc-and-plot.sh $FN
+	/shared/cleaning/scripts/join-to-hrc-and-plot.sh $FN
 
 	mv -v $FN*.png 08_plot_freqs
 done
--- 08_plot_freqs_kgp_detect_POP.sh
+++ 08_plot_freqs_kgp_detect_POP.sh
@@ -1,14 +1,14 @@
 mkdir -p 08_plot_freqs
 for FN in `ls 05_gwas_combined/*.gwas`
 do
-	POP=`../../scripts/find-population.sh $FN`
+	POP=`/shared/cleaning/scripts/find-population.sh $FN`
 	if [ "$POP" == "???" ]
 	then
 		echo "Unable to detect population for: $FN"
 		echo "SKIP!"
 	else
 		echo "Check $FN with KGP $POP"
-		../../scripts/join-to-kgp-and-plot.sh $FN $POP
+		/shared/cleaning/scripts/join-to-kgp-and-plot.sh $FN $POP
 
 		mv -v $FN*.png 08_plot_freqs
 	fi
